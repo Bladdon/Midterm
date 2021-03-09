dca3515fff40e3c39a450b1c05d1943617045a69
Hadi <chaabanh@umich.edu>
2021-03-09 13:01:53 -0500

    CHANGED FIRST INT

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,7 +2,7 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 30;
+        int first = 32;
         int second = 42;
 
         System.out.println("Enter two numbers: " + first + " " + second);
